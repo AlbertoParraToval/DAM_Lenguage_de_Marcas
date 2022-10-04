97c5fec6664d5d4faa1c91b5dd389737967d2208
Alberto Parra Toval <91873665+AlbertoParraToval@users.noreply.github.com>
2022-10-04 18:37:13 +0200

    Add files via upload

---
+++ WebCamPage/README.md
@@ -0,0 +1,2 @@
+# WebCamPage
+![image](https://user-images.githubusercontent.com/91873665/145476285-1af93fd1-18a7-488a-ba4f-1f5423f8f714.png)
---
+++ WebCamPage/javascripts/javascript.js
@@ -0,0 +1,9 @@
+/* BARRA DE NAVEGACION RESPONSIVE*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
